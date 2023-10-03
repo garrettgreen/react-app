f2a803c70e79e96fd8ef1077f10b4c8fbde160b0
Garrett GREEN <garrettgreen@Garretts-MBP-6.lan>
2023-10-03 10:59:23 -0700

    added submitting form

--- react-frontend/src/Form.js
+++ react-frontend/src/Form.js
@@ -1,7 +1,7 @@
 // src/Form.js
 import React, {useState} from "react";
 
-function Form() {
+function Form(props) {
     const [person, setPerson] = useState(
        {
           name: "",
@@ -19,7 +19,13 @@ function Form() {
            setPerson(
              {name: value, job: person['job']}   
            );
-      }
+    }
+
+    function submitForm() {
+        props.handleSubmit(person);
+        setPerson({name: '', job: ''});
+    }
+
 
     return (
         <form>
@@ -37,6 +43,11 @@ function Form() {
             id="job"
             value={person.job}
             onChange={handleChange} />
+          <input
+            type="button"
+            value="Submit"
+            onClick={submitForm}/>
+            
         </form>
     );
   
