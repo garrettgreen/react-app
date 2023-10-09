9e5d306226be560b2429bbbc59608270dab40a56
Garrett GREEN <garrettgreen@Garretts-MBP-6.lan>
2023-10-09 08:46:50 -0700

    201 status

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -8,6 +8,22 @@ function MyApp() {
   const [characters, setCharacters] = useState([
     ]); 
 
+    async function makePostCall(person) {
+      try {
+        const response = await axios.post('http://localhost:8000/users', person);
+  
+        if (response.status === 201) {
+          // Check for a 201 status code and update the state
+          setCharacters([...characters, person]);
+        }
+  
+        return response;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    }
+
     useEffect(() => {
       fetchAll().then( result => {
          if (result)
@@ -34,9 +50,12 @@ function MyApp() {
 	  setCharacters(updated);
 	}
 
-    function updateList(person) {
-        setCharacters([...characters, person]);
-    }
+    function updateList (person) { 
+      makePostCall(person).then( result => {
+      if (result && result.status === 200)
+        setCharacters([...characters, person] );
+      });
+ }
     
   return (
 	<div className="container">
